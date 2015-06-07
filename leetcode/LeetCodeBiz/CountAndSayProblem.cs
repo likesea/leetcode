ca5d5bd1290e30e3778859588bf6c0af886c369b
likesea <kukuyanbin@163.com>
2015-06-07 23:05:23 +0800

    leetcode first commit

---
+++ leetcode/LeetCodeBiz/CountAndSayProblem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeBiz
+{
+    public class CountAndSayProblem
+    {
+        public  string CountAndSay(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+            else
+                return Say(CountAndSay(n - 1));
+        }
+
+        private  string Say(string s)
+        {
+            int count = 0;
+            string result = string.Empty;
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == s[j] && i != s.Length - 1)
+                {
+                    count++;
+                }
+                else if (s[i] == s[j] && i == s.Length - 1)
+                {
+                    count++;
+                    result += count.ToString() + s[j];
+                }
+                else if (s[i] != s[j] && i == s.Length - 1)
+                {
+                    result += count.ToString() + s[j];
+                    result += "1" + s[i].ToString();
+                }
+                else if (s[i] != s[j] && i != s.Length - 1)
+                {
+                    result += count.ToString() + s[j];
+                    count = 1;
+                    j = i;
+                }
+            }
+            return result;
+        }
+    }
+}
