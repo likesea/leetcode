cdee8a9959763ca32ab273b0b82aafae8c789c39
likesea <kukuyanbin@163.com>
2015-09-09 22:25:16 +0800

    添加并查集

---
+++ leetcode/LeetCodeBiz/MergeFindSet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeBiz
+{
+    /// <summary>
+    /// 并查集
+    /// </summary>
+    public class MergeFindSet<T> where T : IEquatable<int>
+    {
+        private Dictionary<T, T> dic =new Dictionary<T, T>();
+        private Dictionary<T, int> rank = new Dictionary<T,int>();  
+
+        public void Add(T item)
+        {
+            dic.Add(item,default(T));
+            rank.Add(item,0);
+        }
+
+        public T FindParent(T item)
+        {
+            if (dic[item].Equals(item))
+            {
+                return item;
+            }
+            else
+            {
+                return dic[item] = FindParent(item);
+            }
+        }
+
+        public void Unite(T x, T y)
+        {
+            x = FindParent(x);
+            y = FindParent(y);
+            if (x.Equals(y)) return;
+            if (rank[x] > rank[y])
+            {
+                dic[y] = x;
+            }
+            else
+            {
+                dic[x] = y;
+                if (rank[x] == rank[y]) rank[y]++;
+            }
+        }
+
+        public bool SameParent(T x, T y)
+        {
+            return FindParent(x).Equals(FindParent(y));
+        }
+    }
+}
