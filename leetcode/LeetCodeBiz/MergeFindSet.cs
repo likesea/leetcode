a3cdb263be518bbfba7675b01b7e218869acadca
likesea <kukuyanbin@163.com>
2015-09-09 22:27:49 +0800

    refator

--- leetcode/LeetCodeBiz/MergeFindSet.cs
+++ leetcode/LeetCodeBiz/MergeFindSet.cs
@@ -11,25 +11,22 @@ namespace LeetCodeBiz
     /// </summary>
     public class MergeFindSet<T> where T : IEquatable<int>
     {
-        private Dictionary<T, T> dic =new Dictionary<T, T>();
-        private Dictionary<T, int> rank = new Dictionary<T,int>();  
+        private readonly Dictionary<T, T> _dic =new Dictionary<T, T>();
+        private readonly Dictionary<T, int> _rank = new Dictionary<T,int>();  
 
         public void Add(T item)
         {
-            dic.Add(item,default(T));
-            rank.Add(item,0);
+            _dic.Add(item,default(T));
+            _rank.Add(item,0);
         }
 
         public T FindParent(T item)
         {
-            if (dic[item].Equals(item))
+            if (_dic[item].Equals(item))
             {
                 return item;
             }
-            else
-            {
-                return dic[item] = FindParent(item);
-            }
+            return _dic[item] = FindParent(item);
         }
 
         public void Unite(T x, T y)
@@ -37,14 +34,14 @@ namespace LeetCodeBiz
             x = FindParent(x);
             y = FindParent(y);
             if (x.Equals(y)) return;
-            if (rank[x] > rank[y])
+            if (_rank[x] > _rank[y])
             {
-                dic[y] = x;
+                _dic[y] = x;
             }
             else
             {
-                dic[x] = y;
-                if (rank[x] == rank[y]) rank[y]++;
+                _dic[x] = y;
+                if (_rank[x] == _rank[y]) _rank[y]++;
             }
         }
 
